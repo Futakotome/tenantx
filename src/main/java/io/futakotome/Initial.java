d4779d6de5e6315217cde40c10b515e15754f92d
Administrator <983528987@qq.com>
2021-02-23 17:31:17 +0800

    advice

--- src/main/java/io/futakotome/Initial.java
+++ src/main/java/io/futakotome/Initial.java
@@ -2,22 +2,23 @@ package io.futakotome;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.description.ModifierReviewable;
 
 import java.lang.instrument.Instrumentation;
 
-import static net.bytebuddy.matcher.ElementMatchers.isMethod;
-import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class Initial {
     public static void premain(String args, Instrumentation instrumentation) {
         System.out.println("This is the tenant extractor.");
 
         new AgentBuilder.Default()
-                .type(ElementMatchers.nameStartsWith("javax.servlet"))
-                .transform((builder, typeDescription, classLoader, javaModule) -> builder.visit(Advice
-                        .to(MyAdvice.class)
-                        .on(isMethod().and(named("getParameterMap")))))
+                .type(hasSuperType(nameStartsWith("org.springframework.web.servlet")))
+                .transform((builder, typeDescription, classLoader, javaModule) ->
+                        builder.visit(Advice
+                                .to(MyAdvice.class)
+                                .on(isMethod()))
+                )
                 .installOn(instrumentation);
 
     }
--- src/main/java/io/futakotome/MyAdvice.java
+++ src/main/java/io/futakotome/MyAdvice.java
@@ -2,15 +2,25 @@ package io.futakotome;
 
 import net.bytebuddy.asm.Advice;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class MyAdvice {
 
     @Advice.OnMethodEnter
-    public static void enter(@Advice.This Object obj,
-                             @Advice.AllArguments Object[] args,
-                             @Advice.Return Object result) {
-        System.out.println(obj);
+    public static void enter(@Advice.Origin Method method,
+                             @Advice.AllArguments Object[] args) {
+        System.out.println("enter");
+        System.out.println(method.toString());
+        System.out.println(Arrays.toString(args));
+    }
+
+    @Advice.OnMethodExit
+    public static void exit(@Advice.Origin Method method,
+                            @Advice.AllArguments Object[] args,
+                            @Advice.Return Object result) {
+        System.out.println("exit");
+        System.out.println(method.toString());
         System.out.println(Arrays.toString(args));
         System.out.println(result);
     }
